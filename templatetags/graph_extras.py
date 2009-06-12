89036dbf7d183ba820d4a55fa6d84490fa11e611
Jeff Larson <thejefflarson@gmail.com>
2009-06-12 12:06:37 -0400

    saving this wrong path exploration

--- templatetags/graph_extras.py
+++ templatetags/graph_extras.py
@@ -14,15 +14,24 @@ def merge(reps):
 @register.inclusion_tag('better_represent/graph.html')
 def draw_graph(reps, num=3, pronoun=''):
     list = {}
+    by_day = []
+    max = 0
     try:
         reps = reps[:num]
     except:
         reps = [reps]
     for rep in reps:
         list[str(rep)] = rep.stats_by_day()
-    return {'json': "%s" % DateEncoder().encode(list), 'data': merge(reps), 'pronoun': pronoun }
+        for day in rep.stats_by_day():
+            day['rep'] = rep
+            by_day.append(day)
+            if max < day['num_stats']: 
+                max = day['num_stats']
+        by_day.sort(key=lambda x: x['date'])
+    return {'json': "%s" % DateEncoder().encode(list), 'data': merge(reps), 'pronoun': pronoun, 'by_day':by_day, 'max':max }
 
 @register.filter
 def max_by_key(value, arg):
-    return max([i[arg] for i in value])
+#    return max([i[arg] for i in value])
+    pass
 
